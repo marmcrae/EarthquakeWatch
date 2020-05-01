df9132110a3d711c8ca2ca42ae9234d75ca54549
marmcrae <mcrae.marquie@gmail.com>
2020-05-01 01:00:44 -1000

    Initial commit

---
+++ app/src/main/java/com/project/earthquakewatch/Util/Constants.java
@@ -0,0 +1,18 @@
+package com.project.earthquakewatch.Util;
+
+import java.util.Random;
+
+public class Constants {
+
+    public static final String URL = "ht" +
+            "" +
+            "tps://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
+
+    public static final int LIMIT= 30;
+
+
+
+    public static  int randomInt(int max, int min) {
+        return new Random().nextInt(max-min) + min;
+    }
+}
